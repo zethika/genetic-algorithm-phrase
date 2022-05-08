c0ef9da1f1cf1b353bb87ef1b57bb379745614cd
zethika <zethika594@gmail.com>
2022-05-08 18:41:11 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Controls from "@/components/Controls.vue";
+</script>
+
+<template>
+    <div class="flex">
+        <div class="flex-grow"></div>
+        <Controls />
+    </div>
+</template>
