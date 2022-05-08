4c3a874a717c6f65c66cb2758425f91bd8113444
zethika <zethika594@gmail.com>
2022-05-08 19:13:43 +0200

    Feature: Added stepper logic to controls store

--- src/store/controls.ts
+++ src/store/controls.ts
@@ -3,10 +3,29 @@ import { defineStore } from 'pinia'
 export const useControlsStore = defineStore('controls', {
     state: () => {
         return {
+            step: 0,
+            stepper: undefined as undefined|NodeJS.Timer,
             running: false,
             phrase: 'To be or not to be.',
             population: 200,
             mutationRate: 10
         }
     },
+    actions: {
+        startStepper(){
+            if(this.stepper !== undefined)
+                return;
+
+            this.stepper = setInterval(() => {
+                this.step++;
+            },1000/30)
+        },
+        stopStepper(){
+            if(this.stepper === undefined)
+                return;
+
+            clearInterval(this.stepper)
+            this.stepper = undefined;
+        }
+    }
 })
