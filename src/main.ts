1208803f631e9691881954fb5dc410a320d6cb79
zethika <zethika594@gmail.com>
2022-05-08 18:42:56 +0200

    Feature: Pinia support

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import './index.css'
 
-createApp(App).mount('#app')
+import { createPinia } from 'pinia'
+
+createApp(App)
+    .use(createPinia())
+    .mount('#app')
