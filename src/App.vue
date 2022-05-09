bc66229db95647509337fff530fed53fae93a2dc
zethika <zethika594@gmail.com>
2022-05-09 07:41:16 +0200

    Feature: Added control change logic

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import Controls from "@/components/Controls.vue";
 import {useControlsStore} from "@/store/controls";
-import {onMounted, reactive, watch} from "vue";
+import {reactive, watch} from "vue";
 import Population from "@/classes/Population";
 const store = useControlsStore();
 const allowedSpeed = 1000/30;
-const population = new Population(
+let population = new Population(
     store.population,
     store.mutationRate,
     store.phrase
@@ -17,10 +17,6 @@ const state = reactive({
     generations: 0,
     averageFitness: 0,
     bestFitness: 0
-})
-
-onMounted(() => {
-    console.log(population)
 })
 
 function runIteration(){
@@ -56,18 +52,33 @@ watch(() => store.running, () => {
         runIteration()
 })
 
+watch([() => store.population, () => store.phrase, () => store.mutationRate], () => {
+    store.running = false;
+    state.bestPhrase = ''
+    state.phrases = [] as Array<string>
+    state.generations = 0
+    state.averageFitness = 0
+    state.bestFitness = 0
+
+    population = new Population(
+        store.population,
+        store.mutationRate,
+        store.phrase
+    );
+})
+
 </script>
 
 <template>
     <div class="flex w-screen h-screen">
         <div class="flex-grow p-4">
             <h1 class="text-2xl mb-4">Population</h1>
-            <p class="text-xl">Best fit: {{state.bestPhrase}}</p>
+            <p class="text-xl whitespace-nowrap">Best fit: {{state.bestPhrase}}</p>
             <p>Generation: {{state.generations}}</p>
             <p>Average fitness: {{state.averageFitness.toFixed(2)}}%</p>
             <p>Best fitness: {{state.bestFitness}}</p>
             <p class="mt-4">Phrases:</p>
-            <p v-for="phrase in state.phrases">{{phrase}}</p>
+            <p v-for="phrase in state.phrases.slice(0,50)" class="whitespace-nowrap">{{phrase}}</p>
         </div>
         <div class="p-4 bg-blue-500 text-white">
             <Controls />
